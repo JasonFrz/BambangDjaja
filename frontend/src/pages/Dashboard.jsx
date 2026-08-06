142b39b36e0ab2d93224230538bfcd8e021411b5
JasonFrz <jasonj2aj@gmail.com>
2026-08-06 14:53:44 +0700

    Update Dashboard.jsx

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1219,7 +1219,7 @@ const Dashboard = () => {
             className="layout"
             layouts={lockedLayouts}
             breakpoints={{ lg: 1200, md: 996, sm: 768, xs: 480, xxs: 0 }}
-            cols={{ lg: 12, md: 10, sm: 6, xs: 4, xxs: 2 }}
+            cols={{ lg: 12, md: 10, sm: 6, xs: 2, xxs: 1 }}
             rowHeight={50}
             onLayoutChange={handleLayoutChange}
             draggableHandle=".drag-handle"
